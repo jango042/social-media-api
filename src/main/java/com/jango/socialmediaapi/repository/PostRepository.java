fe618c792b7f717bb0959f2042d118f11ea98174
jango042 <jango042@yahoo.com>
2023-10-25 22:33:19 +0100

    adding repository interfaces

---
+++ src/main/java/com/jango/socialmediaapi/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.jango.socialmediaapi.repository;
+
+import com.jango.socialmediaapi.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
